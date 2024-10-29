39ff06b8de61ed9dd9d2291a909e372dc4aad9e7
LuckySeven <131405138+LuckySevenn@users.noreply.github.com>
2024-10-29 18:05:25 -0500

    cambios laura

--- src/pages/Menu/Inicio.jsx
+++ src/pages/Menu/Inicio.jsx
@@ -4,14 +4,25 @@ import {Navbar} from "../Componenetes/Navbar"; // Asegúrate de que la ruta sea 
 import Cinnamoroll from "../Cinnamoroll/Cinnamoroll"; // Asegúrate de que la ruta sea correcta
 import { Canvas } from "@react-three/fiber";
 import Gaia from "../../Modelos3D/Gaia";
+import Lights from "../../pages/lights/Lights";
 import "./Inicio.css"
 
+<<<<<<< HEAD
 const Inicio = () => {
+=======
+
+const Menu = () => {
+>>>>>>> 0ea1773 (Biodiversity changes)
     return (
         <>
             <Navbar/>
             <div className="canvas-container">
+<<<<<<< HEAD
                 <Gaia name={"Inicio"} />
+=======
+                <Lights />
+                <Gaia />
+>>>>>>> 0ea1773 (Biodiversity changes)
             </div>
         </>
     );
