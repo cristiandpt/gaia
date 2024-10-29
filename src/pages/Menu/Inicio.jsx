2ebc9f26e7378b046afbeb866e0908ade919042f
LuckySeven <131405138+LuckySevenn@users.noreply.github.com>
2024-10-29 11:25:23 -0500

    New folders for better comprenhension

--- src/pages/Menu/Inicio.jsx
+++ src/pages/Menu/Inicio.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/no-unknown-property */
 import React from "react";
-import {Navbar} from "../Componenetes/Navbar"; // Asegúrate de que la ruta sea correcta
-import Cinnamoroll from "../Cinnamoroll/Cinnamoroll"; // Asegúrate de que la ruta sea correcta
+import {Navbar} from "../Componenetes/Navbar"; // Asegúrate de que la ruta sea correcta// Asegúrate de que la ruta sea correcta
 import { Canvas } from "@react-three/fiber";
 import Gaia from "../../Modelos3D/Gaia";
 import "./Inicio.css"
