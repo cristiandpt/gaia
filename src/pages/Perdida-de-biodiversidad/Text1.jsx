fb8dd950aac50ff662a2d713d5aa0b4b47f73957
LuckySeven <131405138+LuckySevenn@users.noreply.github.com>
2024-10-29 21:17:34 -0500

    Changes in biodiversity

---
+++ src/pages/Perdida-de-biodiversidad/Text1.jsx
@@ -0,0 +1,19 @@
+import "../Perdida-de-biodiversidad/Text1.css";
+
+export default function Text1() {
+  return (
+    <>
+      {/* Contenedor del título y texto */}
+      <div className="container">
+        <h2 className="title">PÉRDIDA DE LA BIODIVERSIDAD</h2>
+        <div className="info-text">
+          La pérdida de biodiversidad es la disminución o extinción de especies, 
+          ecosistemas y recursos genéticos en un área determinada. Ocurre por causas 
+          como la deforestación, contaminación, cambio climático, caza excesiva, y la 
+          expansión urbana o agrícola, lo que fragmenta hábitats y reduce la capacidad 
+          de las especies para sobrevivir y reproducirse.
+        </div>
+      </div>
+    </>
+  );
+}
