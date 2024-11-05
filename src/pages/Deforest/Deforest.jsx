b6336340e5fd8dd776261a4732e4312288357f09
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-11-05 14:15:55 -0500

    Adding the changes in the text component and the deforest component

--- src/pages/Deforest/Deforest.jsx
+++ src/pages/Deforest/Deforest.jsx
@@ -1,8 +1,8 @@
 import { Navbar } from "../../components/Navbar.jsx";
 import Gaia from "../../3D-models/Gaia.jsx";
 import {
-	DeforestationModel1,
-	DeforestationModel2,
+  DeforestationModel1,
+  DeforestationModel2,
 } from "../../3D-models/deforestation/Deforestation.jsx";
 import "./Deforest.css"; // CSS adicional para los textos fijos
 import { useRef } from "react";
@@ -11,41 +11,41 @@ import { Suspense } from "react";
 import { Environment, OrbitControls, useGLTF } from "@react-three/drei";
 
 const DeforestationPage = () => {
-	return (
-		<>
-			<Navbar />
-			<div className="canvas-container">
-				<div className="canvas-container">
-					<Canvas shadows camera={{ position: [0, 0, 15] }}>
-						<ambientLight intensity={0.5} />
-						<directionalLight
-							position={[10, 10, 10]}
-							intensity={0.7}
-							castShadow
-						/>
-						<Suspense fallback={null}>
-							<DeforestationModel1 />
-							<DeforestationModel2 />
-							<OrbitControls enableZoom={false} />
-						</Suspense>
-						<Environment preset="sunset" />
-					</Canvas>
-					<div className="fixed-text">{name}</div>
-				</div>
-				<div className="info-text">
-					<h2>Impacto de la Deforestación</h2>
-					<p>
-						La deforestación causa la pérdida de biodiversidad, el cambio
-						climático y la destrucción de hábitats esenciales.
-					</p>
-					<p>
-						Cada año, millones de hectáreas de bosques son taladas, amenazando
-						la vida de innumerables especies.
-					</p>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Navbar />
+      <div className="canvas-container">
+        <div className="canvas-container">
+          <Canvas shadows camera={{ position: [0, 0, 15] }}>
+            <ambientLight intensity={0.5} />
+            <directionalLight
+              position={[10, 10, 10]}
+              intensity={0.7}
+              castShadow
+            />
+            <Suspense fallback={null}>
+              <DeforestationModel1 />
+              <DeforestationModel2 />
+              <OrbitControls enableZoom={false} />
+            </Suspense>
+            <Environment preset="sunset" />
+          </Canvas>
+          <div className="fixed-text">{name}</div>
+        </div>
+        <div className="info-text">
+          <h2>Impacto de la Deforestación</h2>
+          <p>
+            La deforestación causa la pérdida de biodiversidad, el cambio
+            climático y la destrucción de hábitats esenciales.
+          </p>
+          <p>
+            Cada año, millones de hectáreas de bosques son taladas, amenazando
+            la vida de innumerables especies.
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default DeforestationPage;
