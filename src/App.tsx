8915b9aa1a40345f0bec9b5a54e55831a7ae08f5
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-12-08 12:34:23 -0500

    [EDIT] Prettify the code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes/routes";
+import Home from "./pages/LandingPage/LandingPage";
 
 function App() {
   const router = createBrowserRouter(routes);
-  return <RouterProvider router={router} />;
+  //return <RouterProvider router={router} />;
+  <Home />;
 }
 
 export default App;
--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -14,76 +14,76 @@ const SEL = "custom-section";
 const SECTION_SEL = `.${SEL}`;
 
 const questions = [
-	{
-		text: "Pregunta 1: ¿Cuál de las siguientes prácticas agrícolas ayuda a prevenir la erosión del suelo?",
-		options: [
-			"a) Aumenta la resiliencia de los ecosistemas",
-			"b) Disminuye la estabilidad y funcionalidad de los ecosistemas",
-			"c) Mejora la calidad del agua",
-			"d) Aumenta la producción de alimentos",
-		],
-	},
-	{
-		text: "Pregunta 2: ¿Cuál de las siguientes es una de las principales causas de la deforestación a nivel mundial?",
-		options: [
-			"a) Aumenta la resiliencia de los ecosistemas",
-			"b) Disminuye la estabilidad y funcionalidad de los ecosistemas",
-			"c) Mejora la calidad del agua",
-			"d) Aumenta la producción de alimentos",
-		],
-	},
-	{
-		text: "Pregunta 3: ¿Qué efecto tiene la pérdida de biodiversidad en los ecosistemas?",
-		options: [
-			"a) Aumenta la resiliencia de los ecosistemas",
-			"b) Disminuye la estabilidad y funcionalidad de los ecosistemas",
-			"c) Mejora la calidad del agua",
-			"d) Aumenta la producción de alimentos",
-		],
-	},
+  {
+    text: "Pregunta 1: ¿Cuál de las siguientes prácticas agrícolas ayuda a prevenir la erosión del suelo?",
+    options: [
+      "a) Aumenta la resiliencia de los ecosistemas",
+      "b) Disminuye la estabilidad y funcionalidad de los ecosistemas",
+      "c) Mejora la calidad del agua",
+      "d) Aumenta la producción de alimentos",
+    ],
+  },
+  {
+    text: "Pregunta 2: ¿Cuál de las siguientes es una de las principales causas de la deforestación a nivel mundial?",
+    options: [
+      "a) Aumenta la resiliencia de los ecosistemas",
+      "b) Disminuye la estabilidad y funcionalidad de los ecosistemas",
+      "c) Mejora la calidad del agua",
+      "d) Aumenta la producción de alimentos",
+    ],
+  },
+  {
+    text: "Pregunta 3: ¿Qué efecto tiene la pérdida de biodiversidad en los ecosistemas?",
+    options: [
+      "a) Aumenta la resiliencia de los ecosistemas",
+      "b) Disminuye la estabilidad y funcionalidad de los ecosistemas",
+      "c) Mejora la calidad del agua",
+      "d) Aumenta la producción de alimentos",
+    ],
+  },
 ];
 
 type QuestionType = 1 | 2 | 3;
 
 const generateQuestion = (selectedQuestion: QuestionType) => {
-	switch (selectedQuestion) {
-		case 1:
-			<SoilLayers
-				onHover={(payload) => console.log("")}
-				scale={3}
-				position={[0, 0, 0]}
-			/>;
-			break;
-		case 2:
-		case 3:
-			return <></>;
-	}
+  switch (selectedQuestion) {
+    case 1:
+      <SoilLayers
+        onHover={(payload) => console.log("")}
+        scale={3}
+        position={[0, 0, 0]}
+      />;
+      break;
+    case 2:
+    case 3:
+      return <></>;
+  }
 };
 
 const QuizFlow = () => {
-	return (
-		<Canvas
-			className="h-screen w-screen"
-			shadows
-			camera={{ position: [0, 5, 15] }}
-		>
-			<ambientLight intensity={0.3} /> {/* Ground plane to receive shadows */}
-			<mesh
-				receiveShadow
-				rotation={[-Math.PI / 2, 0, 0]}
-				position={[0, -4.09, 0]}
-			>
-				<planeGeometry args={[500, 500]} />
-				<shadowMaterial opacity={0.7} />
-			</mesh>
-			<Suspense fallback={<Loader />}>{generateQuestion(1)}</Suspense>
-			<QuestionControl />
-			<Environment background files="imagenes/qwantani_dusk_2_2k.hdr" />
-			<Html wrapperClass="div" position={[0, -10, 0]} center>
-				<EmptyingProgressBar duration={100000} />
-			</Html>
-		</Canvas>
-	);
+  return (
+    <Canvas
+      className="h-screen w-screen"
+      shadows
+      camera={{ position: [0, 5, 15] }}
+    >
+      <ambientLight intensity={0.3} /> {/* Ground plane to receive shadows */}
+      <mesh
+        receiveShadow
+        rotation={[-Math.PI / 2, 0, 0]}
+        position={[0, -4.09, 0]}
+      >
+        <planeGeometry args={[500, 500]} />
+        <shadowMaterial opacity={0.7} />
+      </mesh>
+      <Suspense fallback={<Loader />}>{generateQuestion(1)}</Suspense>
+      <QuestionControl />
+      <Environment background files="imagenes/qwantani_dusk_2_2k.hdr" />
+      <Html wrapperClass="div" position={[0, -10, 0]} center>
+        <EmptyingProgressBar duration={100000} />
+      </Html>
+    </Canvas>
+  );
 };
 
 export default QuizFlow;
