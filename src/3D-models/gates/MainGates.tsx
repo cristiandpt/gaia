1a0723c6d696d08f69a8c78dd7d80bb47c027af8
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-12-10 13:05:36 -0500

    [ADD] Posproccessing

--- src/3D-models/gates/MainGates.tsx
+++ src/3D-models/gates/MainGates.tsx
@@ -5,169 +5,169 @@ import { Object3D } from "three";
 import PortalStage from "../../components/Portal";
 
 interface Props {
-	onEnterPortal: (route: string) => void;
-	onExitPortal: () => void;
+  onEnterPortal: (route: string) => void;
+  onExitPortal: () => void;
 }
 
 const MainGates = ({ onEnterPortal, onExitPortal }: Props) => {
-	const { scene } = useGLTF("3D-models/home/Main-gates.glb");
+  const { scene } = useGLTF("3D-models/home/Main-gates.glb");
 
-	scene.traverse((child: Object3D) => {
-		child.castShadow = true;
-		child.receiveShadow = true;
-	});
+  scene.traverse((child: Object3D) => {
+    child.castShadow = true;
+    child.receiveShadow = true;
+  });
 
-	const GenerateCuboideCollider = (
-		position: Vector3,
-		rotation: Euler,
-		onEnteringHandler: () => void,
-		onExitingHandler: () => void,
-	) => (
-		<CuboidCollider
-			args={[0.7, 1.75, 0.25]}
-			sensor
-			position={position}
-			rotation={rotation}
-			onIntersectionEnter={onEnteringHandler}
-			onIntersectionExit={onExitingHandler}
-		/>
-	);
+  const GenerateCuboideCollider = (
+    position: Vector3,
+    rotation: Euler,
+    onEnteringHandler: () => void,
+    onExitingHandler: () => void,
+  ) => (
+    <CuboidCollider
+      args={[0.7, 1.75, 0.25]}
+      sensor
+      position={position}
+      rotation={rotation}
+      onIntersectionEnter={onEnteringHandler}
+      onIntersectionExit={onExitingHandler}
+    />
+  );
 
-	const CollidersDefinition: {
-		position: Vector3;
-		rotation: Euler;
-		onEnteringHandler: () => void;
-		onExitHandler: () => void;
-	}[] = [
-		{
-			position: [2.9, -0.5, -3.8],
-			rotation: [0, -(Math.PI / 3), 0],
-			onEnteringHandler: () => {
-				onEnterPortal("/loss-of-biodiversity");
-			},
-			onExitHandler: onExitPortal,
-		},
-		{
-			position: [1.2, -0.5, -4.7],
-			rotation: [0, -(Math.PI / 18), 0],
-			onEnteringHandler: () => {
-				onEnterPortal("/deforest");
-			},
-			onExitHandler: onExitPortal,
-		},
-		{
-			position: [-1.1, -0.5, -4.6],
-			rotation: [0, Math.PI / 8, 0],
-			onEnteringHandler: () => {
-				onEnterPortal("/erosion");
-			},
-			onExitHandler: onExitPortal,
-		},
-		{
-			position: [-2.25, -0.5, -3.3],
-			rotation: [0, Math.PI / 2.4, 0],
-			onEnteringHandler: () => {
-				onEnterPortal("");
-			},
-			onExitHandler: onExitPortal,
-		},
-	];
+  const CollidersDefinition: {
+    position: Vector3;
+    rotation: Euler;
+    onEnteringHandler: () => void;
+    onExitHandler: () => void;
+  }[] = [
+    {
+      position: [2.9, -0.5, -3.8],
+      rotation: [0, -(Math.PI / 3), 0],
+      onEnteringHandler: () => {
+        onEnterPortal("/loss-of-biodiversity");
+      },
+      onExitHandler: onExitPortal,
+    },
+    {
+      position: [1.2, -0.5, -4.7],
+      rotation: [0, -(Math.PI / 18), 0],
+      onEnteringHandler: () => {
+        onEnterPortal("/deforest");
+      },
+      onExitHandler: onExitPortal,
+    },
+    {
+      position: [-1.1, -0.5, -4.6],
+      rotation: [0, Math.PI / 8, 0],
+      onEnteringHandler: () => {
+        onEnterPortal("/erosion");
+      },
+      onExitHandler: onExitPortal,
+    },
+    {
+      position: [-2.25, -0.5, -3.3],
+      rotation: [0, Math.PI / 2.4, 0],
+      onEnteringHandler: () => {
+        onEnterPortal("");
+      },
+      onExitHandler: onExitPortal,
+    },
+  ];
 
-	const instatieateCubuidsColliders = () =>
-		CollidersDefinition.map((collider) =>
-			GenerateCuboideCollider(
-				collider.position,
-				collider.rotation,
-				collider.onEnteringHandler,
-			),
-		);
+  const instatieateCubuidsColliders = () =>
+    CollidersDefinition.map((collider) =>
+      GenerateCuboideCollider(
+        collider.position,
+        collider.rotation,
+        collider.onEnteringHandler,
+      ),
+    );
 
-	return (
-		<>
-			<RigidBody name="gates" colliders="hull" type="fixed" friction={1.5}>
-				<primitive
-					name="gates"
-					object={scene}
-					position={[0, -2.5, -3]}
-					scale={[0.35, 0.4, 0.5]}
-					rotation={[0, -Math.PI / 2, 0]}
-				/>
-				{instatieateCubuidsColliders()}
-			</RigidBody>
-			<PortalStage
-				texture={"imagenes/near.hdr"}
-				name="Deforestacion"
-				color={"#df8d52"}
-				colorPortal="#2a0f09"
-				active={""}
-				setActive={(her) => console.log(her)}
-				hovered={""}
-				setHovered={(her) => console.log(her)}
-				position={[
-					CollidersDefinition[1].position[0] + 0.05,
-					CollidersDefinition[1].position[1] - 0.8,
-					CollidersDefinition[1].position[2],
-				]}
-				rotation={CollidersDefinition[1].rotation}
-			>
-				<></>
-			</PortalStage>
-			<PortalStage
-				texture={"imagenes/near.hdr"}
-				name="Bioversidad"
-				color={"#df8d52"}
-				colorPortal={"#1e2c01"}
-				active={""}
-				setActive={(her) => console.log(her)}
-				hovered={""}
-				setHovered={(her) => console.log(her)}
-				position={[
-					CollidersDefinition[0].position[0],
-					CollidersDefinition[0].position[1] - 0.8,
-					CollidersDefinition[0].position[2],
-				]}
-				rotation={CollidersDefinition[0].rotation}
-			>
-				<></>
-			</PortalStage>
-			<PortalStage
-				texture={"imagenes/near.hdr"}
-				name="Erosión"
-				color={"#df8d52"}
-				colorPortal={"#3b2d37"}
-				active={""}
-				setActive={(her) => console.log(her)}
-				hovered={""}
-				setHovered={(her) => console.log(her)}
-				position={[
-					CollidersDefinition[2].position[0],
-					CollidersDefinition[2].position[1] - 0.8,
-					CollidersDefinition[2].position[2],
-				]}
-				rotation={CollidersDefinition[2].rotation}
-			>
-				<></>
-			</PortalStage>
-			<PortalStage
-				texture={"imagenes/near.hdr"}
-				name="Ayuda"
-				color={"#df8d52"}
-				colorPortal={"#23180f"}
-				active={""}
-				setActive={(her) => console.log(her)}
-				hovered={""}
-				setHovered={(her) => console.log(her)}
-				position={[
-					CollidersDefinition[3].position[0],
-					CollidersDefinition[3].position[1] - 0.8,
-					CollidersDefinition[3].position[2],
-				]}
-				rotation={CollidersDefinition[3].rotation}
-			>
-				<></>
-			</PortalStage>
-		</>
-	);
+  return (
+    <>
+      <RigidBody name="gates" colliders="hull" type="fixed" friction={1.5}>
+        <primitive
+          name="gates"
+          object={scene}
+          position={[0, -2.5, -3]}
+          scale={[0.35, 0.4, 0.5]}
+          rotation={[0, -Math.PI / 2, 0]}
+        />
+        {instatieateCubuidsColliders()}
+      </RigidBody>
+      <PortalStage
+        texture={"imagenes/near.hdr"}
+        name="Deforestacion"
+        color={"#df8d52"}
+        colorPortal="#2a0f09"
+        active={""}
+        setActive={(her) => console.log(her)}
+        hovered={""}
+        setHovered={(her) => console.log(her)}
+        position={[
+          CollidersDefinition[1].position[0] + 0.05,
+          CollidersDefinition[1].position[1] - 0.8,
+          CollidersDefinition[1].position[2],
+        ]}
+        rotation={CollidersDefinition[1].rotation}
+      >
+        <></>
+      </PortalStage>
+      <PortalStage
+        texture={"imagenes/near.hdr"}
+        name="Bioversidad"
+        color={"#df8d52"}
+        colorPortal={"#1e2c01"}
+        active={""}
+        setActive={(her) => console.log(her)}
+        hovered={""}
+        setHovered={(her) => console.log(her)}
+        position={[
+          CollidersDefinition[0].position[0],
+          CollidersDefinition[0].position[1] - 0.8,
+          CollidersDefinition[0].position[2],
+        ]}
+        rotation={CollidersDefinition[0].rotation}
+      >
+        <></>
+      </PortalStage>
+      <PortalStage
+        texture={"imagenes/near.hdr"}
+        name="Erosión"
+        color={"#df8d52"}
+        colorPortal={"#3b2d37"}
+        active={""}
+        setActive={(her) => console.log(her)}
+        hovered={""}
+        setHovered={(her) => console.log(her)}
+        position={[
+          CollidersDefinition[2].position[0],
+          CollidersDefinition[2].position[1] - 0.8,
+          CollidersDefinition[2].position[2],
+        ]}
+        rotation={CollidersDefinition[2].rotation}
+      >
+        <></>
+      </PortalStage>
+      <PortalStage
+        texture={"imagenes/near.hdr"}
+        name="Ayuda"
+        color={"#df8d52"}
+        colorPortal={"#23180f"}
+        active={""}
+        setActive={(her) => console.log(her)}
+        hovered={""}
+        setHovered={(her) => console.log(her)}
+        position={[
+          CollidersDefinition[3].position[0],
+          CollidersDefinition[3].position[1] - 0.8,
+          CollidersDefinition[3].position[2],
+        ]}
+        rotation={CollidersDefinition[3].rotation}
+      >
+        <></>
+      </PortalStage>
+    </>
+  );
 };
 
 export default MainGates;
--- src/components/main-dialog/MainDialog.tsx
+++ src/components/main-dialog/MainDialog.tsx
@@ -2,39 +2,39 @@ import { Html } from "@react-three/drei";
 import { Vector3 } from "three";
 
 interface Style {
-	[props: string]: string | number;
+  [props: string]: string | number;
 }
 
 interface Props {
-	title: string | null;
-	say: string;
-	position: number[];
-	onClickHandler: () => void;
-	style?: Style;
+  title: string | null;
+  say: string;
+  position: number[];
+  onClickHandler: () => void;
+  style?: Style;
 }
 
 const MainDialog = ({ say, position, onClickHandler, title, style }: Props) => (
-	<Html
-		// Adjust position as needed
-		position={new Vector3(position[0], position[1], position[2])}
-		wrapperClass="html-dialog" // Optional: Add a class for styling
-		center // Center the HTML content
-	>
-		<div
-			className="absolute w-80 b-0 r-0 bg-white/[0.25] rounded-md p-4"
-			style={style}
-		>
-			<p className="text-5xl ms-0 text-[#000000] font-bold">{title}</p>
-			{/* You can add a title if needed */}
-			<p className="text-white text-base">{say}</p>
-			<button
-				className="bg-red-500 text-white rounded-md p-2 px-4"
-				onClick={(e) => onClickHandler()}
-			>
-				Explorar
-			</button>
-		</div>
-	</Html>
+  <Html
+    // Adjust position as needed
+    position={new Vector3(position[0], position[1], position[2])}
+    wrapperClass="html-dialog" // Optional: Add a class for styling
+    center // Center the HTML content
+  >
+    <div
+      className="absolute w-80 b-0 r-0 bg-white/[0.25] rounded-md p-4"
+      style={style}
+    >
+      <p className="text-5xl ms-0 text-[#000000] font-bold">{title}</p>
+      {/* You can add a title if needed */}
+      <p className="text-white text-base">{say}</p>
+      <button
+        className="bg-red-500 text-white rounded-md p-2 px-4"
+        onClick={(e) => onClickHandler()}
+      >
+        Explorar
+      </button>
+    </div>
+  </Html>
 );
 
 export default MainDialog;
