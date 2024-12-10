821121876c35651ad509502ac545053157ed422d
JhoanLeon <leon.jhoanf@gmail.com>
2024-12-10 10:46:18 -0500

    funcionamiento firebase

--- src/components/questions/GaiaObjectCapture.tsx
+++ src/components/questions/GaiaObjectCapture.tsx
@@ -22,7 +22,6 @@ import useAuthStore from "../../stores/use-auth-store";
 import { doc, setDoc } from "firebase/firestore"; // Para manipular documentos
 import { db } from "../../../firebase.config"; // Ruta a tu configuración de Firebase
 
-
 function GaiaModel() {
   const { scene } = useGLTF("3D-models/Gaia4.glb");
   const gaiaRef = useRef();
@@ -31,70 +30,74 @@ function GaiaModel() {
 
   // Movimiento oscilatorio y control con teclas
   useFrame(() => {
-      if (gaiaRef.current) {
-          const gaiaPosition = gaiaRef.current.translation();
-          const newY =
-              Math.sin(Date.now() * 0.002) * 0.1; // Movimiento oscilatorio
+    if (gaiaRef.current) {
+      const gaiaPosition = gaiaRef.current.translation();
+      const newY = Math.sin(Date.now() * 0.002) * 0.1; // Movimiento oscilatorio
 
-          gaiaRef.current.setTranslation(
-              { x: gaiaPosition.x, y: newY, z: gaiaPosition.z },
-              true,
-          );
-      }
+      gaiaRef.current.setTranslation(
+        { x: gaiaPosition.x, y: newY, z: gaiaPosition.z },
+        true,
+      );
+    }
   });
 
-  const handleKeyDown = useCallback(
-      (e) => {
-          if (!gaiaRef.current) return;
-          const translation = gaiaRef.current.translation();
-          switch (e.key) {
-              case "ArrowLeft":
-                  gaiaRef.current.setTranslation(
-                      { x: translation.x - 0.5, y: translation.y, z: translation.z },
-                      true,
-                  );
-                  break;
-              case "ArrowRight":
-                  gaiaRef.current.setTranslation(
-                      { x: translation.x + 0.5, y: translation.y, z: translation.z },
-                      true,
-                  );
-                  break;
-              default:
-                  break;
-          }
-      },
-      [],
-  );
+  const handleKeyDown = useCallback((e) => {
+    if (!gaiaRef.current) return;
+    const translation = gaiaRef.current.translation();
+    switch (e.key) {
+      case "ArrowLeft":
+        gaiaRef.current.setTranslation(
+          { x: translation.x - 0.5, y: translation.y, z: translation.z },
+          true,
+        );
+        break;
+      case "ArrowRight":
+        gaiaRef.current.setTranslation(
+          { x: translation.x + 0.5, y: translation.y, z: translation.z },
+          true,
+        );
+        break;
+      default:
+        break;
+    }
+  }, []);
 
   useEffect(() => {
-      document.addEventListener("keydown", handleKeyDown);
-      return () => document.removeEventListener("keydown", handleKeyDown);
+    document.addEventListener("keydown", handleKeyDown);
+    return () => document.removeEventListener("keydown", handleKeyDown);
   }, [handleKeyDown]);
 
-  const handleCollision = useCallback((event) => {
+  const handleCollision = useCallback(
+    (event) => {
       const colliderName = event.collider.userData.name;
-      if (colliderName === 'Drop' || colliderName === 'WaterCan') {
-          increaseScore(20);
-      } else if (colliderName === 'Trowel' || colliderName === 'HatChet') {
-          setBadCollisions((prev) => prev + 1);
-          if (badCollisions + 1 >= 2) {
-              alert("¡Juego terminado! Gaia ha tocado demasiados objetos malos.");
-              // Lógica para terminar el juego o reiniciar
-          }
+      if (colliderName === "Drop" || colliderName === "WaterCan") {
+        increaseScore(20);
+      } else if (colliderName === "Trowel" || colliderName === "HatChet") {
+        setBadCollisions((prev) => prev + 1);
+        if (badCollisions + 1 >= 2) {
+          alert("¡Juego terminado! Gaia ha tocado demasiados objetos malos.");
+          // Lógica para terminar el juego o reiniciar
+        }
       }
-  }, [badCollisions, increaseScore]);
+    },
+    [badCollisions, increaseScore],
+  );
 
   return (
-      <RigidBody
-          ref={gaiaRef}
-          colliders="cuboid"
-          type="fixed"
-          gravityScale={0}
-          onCollisionEnter={() => increaseScore(20)}
-         >
-          <primitive object={scene} scale={[5, 5, 5]} position={[0, 0, 1]} rotation={[0, -Math.PI / 2, 0]} />
-      </RigidBody>
+    <RigidBody
+      ref={gaiaRef}
+      colliders="cuboid"
+      type="fixed"
+      gravityScale={0}
+      onCollisionEnter={() => increaseScore(20)}
+    >
+      <primitive
+        object={scene}
+        scale={[5, 5, 5]}
+        position={[0, 0, 1]}
+        rotation={[0, -Math.PI / 2, 0]}
+      />
+    </RigidBody>
   );
 }
 
@@ -104,44 +107,43 @@ const RandomizeComponent = () => {
   const [instances, setInstances] = useState([]);
 
   useEffect(() => {
-      const interval = setInterval(() => {
-          const randomX = (Math.random() - 0.5) * 60;
-          const componentType = Math.floor(Math.random() * 4) + 1;
-          const newInstance = {
-              component: ModelFactory(componentType, [randomX, 15, 1]),
-              id: Date.now(),
-          };
-          setInstances((prev) => [...prev, newInstance]);
+    const interval = setInterval(() => {
+      const randomX = (Math.random() - 0.5) * 60;
+      const componentType = Math.floor(Math.random() * 4) + 1;
+      const newInstance = {
+        component: ModelFactory(componentType, [randomX, 15, 1]),
+        id: Date.now(),
+      };
+      setInstances((prev) => [...prev, newInstance]);
 
-          setTimeout(() => {
-              setInstances((prev) => prev.filter((i) => i.id !== newInstance.id));
-          }, 5000);
-      }, 2000);
+      setTimeout(() => {
+        setInstances((prev) => prev.filter((i) => i.id !== newInstance.id));
+      }, 5000);
+    }, 2000);
 
-      return () => clearInterval(interval);
+    return () => clearInterval(interval);
   }, [size]);
 
   return instances.map((c) => (
-      <RigidBody key={c.id} userData={{ name: c.component.type.name }}>
-          {c.component}
-      </RigidBody>
+    <RigidBody key={c.id} userData={{ name: c.component.type.name }}>
+      {c.component}
+    </RigidBody>
   ));
 };
-
 
 // Factoría para los modelos
 const ModelFactory = (index, position) => {
   switch (index) {
-      case 1:
-          return <Drop position={position} scale={60} />;
-      case 2:
-          return <Trowel position={position} scale={20} />;
-      case 3:
-          return <HatChet position={position} scale={20} />;
-      case 4:
-          return <WaterCan position={position} scale={15} />;
-      default:
-          return null;
+    case 1:
+      return <Drop position={position} scale={60} />;
+    case 2:
+      return <Trowel position={position} scale={20} />;
+    case 3:
+      return <HatChet position={position} scale={20} />;
+    case 4:
+      return <WaterCan position={position} scale={15} />;
+    default:
+      return null;
   }
 };
 
@@ -152,143 +154,145 @@ const GaiaObjectCapture = () => {
   const [timeLeft, setTimeLeft] = useState(30); // Tiempo en segundos
   const [gameOver, setGameOver] = useState(false);
   const [finalScore, setFinalScore] = useState(0);
-  const [level, setLevel] = useState('');
+  const [level, setLevel] = useState("");
 
   useEffect(() => {
-      const timer = setInterval(() => {
-          setTimeLeft((prevTime) => {
-              if (prevTime > 1) {
-                  return prevTime - 1;
-              } else {
-                  clearInterval(timer);
-                  setFinalScore(score);
-                  setGameOver(true);
-                  showAlert();
-                  return 0;
-              }
-          });
-      }, 1000);
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime > 1) {
+          return prevTime - 1;
+        } else {
+          clearInterval(timer);
+          setFinalScore(score);
+          setGameOver(true);
+          showAlert();
+          return 0;
+        }
+      });
+    }, 1000);
 
-      return () => clearInterval(timer);
+    return () => clearInterval(timer);
   }, [score]);
 
   useEffect(() => {
-      if (gameOver) {
-          if (score < 600) {
-              setLevel('Principiante');
-          } else if (score < 900) {
-              setLevel('Cobre');
-          } else if (score < 1500) {
-              setLevel('Plata');
-          } else {
-              setLevel('Oro');
-          }
+      if (score < 600) {
+        setLevel("Principiante");
+      } else if (score < 900) {
+        setLevel("Cobre");
+      } else if (score < 1500) {
+        setLevel("Plata");
+      } else {
+        setLevel("Oro");
       }
   }, [gameOver, score]);
 
   const showAlert = async () => {
-    const message = `Nombre: ${user ? user.displayName : 'Desconocido'}\nPuntuación final: ${score}\nNivel alcanzado: ${level}\n\n¿Quieres jugar de nuevo o volver al inicio?`;
+    const message = `Nombre: ${user ? user.displayName : "Desconocido"}\nPuntuación final: ${score}\nNivel alcanzado: ${level}\n\n¿Quieres jugar de nuevo o volver al inicio?`;
     const playAgain = window.confirm(message);
 
     if (user) {
-        try {
-            const docRef = doc(db, "scores", user.uid); // Usar el UID del usuario como ID del documento
-            await setDoc(docRef, {
-                displayName: user.displayName || "Desconocido",
-                finalScore,
-                level,
-                timestamp: new Date(),
-            });
-            console.log("Puntaje guardado exitosamente en Firebase");
-        } catch (error) {
-            console.error("Error al guardar el puntaje en Firebase:", error);
-        }
+      try {
+        const docRef = doc(db, "scores", user.uid); // Usar el UID del usuario como ID del documento
+        await setDoc(docRef, {
+          displayName: user.displayName || "Desconocido",
+          score,
+          level,
+          timestamp: new Date(),
+        });
+        console.log("Puntaje guardado exitosamente en Firebase");
+      } catch (error) {
+        console.error("Error al guardar el puntaje en Firebase:", error);
+      }
     } else {
-        console.log("No se puede guardar el puntaje: usuario no autenticado");
+      console.log("No se puede guardar el puntaje: usuario no autenticado");
     }
 
     if (playAgain) {
-        // Recargar la página para reiniciar el juego
-        window.location.reload();
+      // Recargar la página para reiniciar el juego
+      window.location.reload();
     } else {
-        // Redirigir al inicio (ajusta la ruta según tu aplicación)
-        window.location.href = "/";
+      // Redirigir al inicio (ajusta la ruta según tu aplicación)
+      window.location.href = "/";
     }
-};
+  };
 
-
-  
-
-const moveGaia = useCallback(
-  (direction) => {
-      if (!gaiaRef.current) return;
-      const translation = gaiaRef.current.translation();
-      if (direction === "left") {
-          gaiaRef.current.setTranslation(
-              { x: translation.x - 0.5, y: translation.y, z: translation.z },
-              true
-          );
-      } else if (direction === "right") {
-          gaiaRef.current.setTranslation(
-              { x: translation.x + 0.5, y: translation.y, z: translation.z },
-              true
-          );
-      }
-  },
-  []
-);
+  const moveGaia = useCallback((direction) => {
+    if (!gaiaRef.current) return;
+    const translation = gaiaRef.current.translation();
+    if (direction === "left") {
+      gaiaRef.current.setTranslation(
+        { x: translation.x - 0.5, y: translation.y, z: translation.z },
+        true,
+      );
+    } else if (direction === "right") {
+      gaiaRef.current.setTranslation(
+        { x: translation.x + 0.5, y: translation.y, z: translation.z },
+        true,
+      );
+    }
+  }, []);
 
   const resetGame = () => {
-      resetScore();
-      setGameOver(false);
-      setFinalScore(0);
-      setLevel('');
-      setTimeLeft(30);
+    resetScore();
+    setGameOver(false);
+    setFinalScore(0);
+    setLevel("");
+    setTimeLeft(30);
   };
 
   return (
-      <div style={{ height: "100vh", width: "100vw" }}> (
-              <Canvas camera={{ position: [0, 5, 30] }} shadows>
-                  <Html position={[-20, 17, 0]} scale={[3]}>
-                      <p style={{ fontSize: "3rem", color: "white" }}>Nombre: {user ? user.displayName : 'Desconocido'}</p>
-                  </Html>
-                  <Html position={[12, 17, 0]} scale={[5]}>
-                      <p style={{ fontSize: "3rem", color: "white" }}>Tiempo restante: {timeLeft} s</p>
-                  </Html>
-                  <Html position={[0, 17, 0]} scale={[5]}>
-                      <p>Puntuacion: {score}</p>
-                  </Html>
-                  <Html
-    center
-    style={{ width: "90vw", display: "flex", justifyContent: "space-between" }}
->
-    <FaArrowCircleLeft
-        style={{ height: "7rem", width: "7rem", cursor: "pointer" }}
-        onClick={() => handleKeyDown({ key: "ArrowLeft" })} // Simula la tecla "ArrowLeft"
-    />
-    <FaArrowCircleRight
-        style={{ height: "7rem", width: "7rem", cursor: "pointer" }}
-        onClick={() => handleKeyDown({ key: "ArrowRight" })} // Simula la tecla "ArrowRight"
-    />
-</Html>
+    <div style={{ height: "100vh", width: "100vw" }}>
+      {" "}
+      (
+      <Canvas camera={{ position: [0, 5, 30] }} shadows>
+        <Html position={[-20, 17, 0]} scale={[3]}>
+          <p style={{ fontSize: "3rem", color: "white" }}>
+            Nombre: {user ? user.displayName : "Desconocido"}
+          </p>
+        </Html>
+        <Html position={[12, 17, 0]} scale={[5]}>
+          <p style={{ fontSize: "3rem", color: "white" }}>
+            Tiempo restante: {timeLeft} s
+          </p>
+        </Html>
+        <Html position={[0, 17, 0]} scale={[5]}>
+          <p>Puntuacion: {score}</p>
+        </Html>
+        <Html
+          center
+          style={{
+            width: "90vw",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <FaArrowCircleLeft
+            style={{ height: "7rem", width: "7rem", cursor: "pointer" }}
+            onClick={() => handleKeyDown({ key: "ArrowLeft" })} // Simula la tecla "ArrowLeft"
+          />
+          <FaArrowCircleRight
+            style={{ height: "7rem", width: "7rem", cursor: "pointer" }}
+            onClick={() => handleKeyDown({ key: "ArrowRight" })} // Simula la tecla "ArrowRight"
+          />
+        </Html>
 
-                  <Lights />
-                  <Environment
-                      files="imagenes/satara_night_no_lamps_4k.hdr"
-                      background
-                      backgroundIntensity={1}
-                  />
-                  <OrbitControls />
-                  <Suspense fallback={null}>
-                      <Physics>
-                          <RandomizeComponent />
-                          <GaiaCapturePlane position={[0, -11, 2]} scale={[10, 10, 10]} />
-                          <GaiaModel />
-                      </Physics>
-                  </Suspense>
-              </Canvas>
-          )
-      </div>
+        <Lights />
+        <Environment
+          files="imagenes/satara_night_no_lamps_4k.hdr"
+          background
+          backgroundIntensity={1}
+        />
+        <OrbitControls />
+        <Suspense fallback={null}>
+          <Physics>
+            <RandomizeComponent />
+            <GaiaCapturePlane position={[0, -11, 2]} scale={[10, 10, 10]} />
+            <GaiaModel />
+          </Physics>
+        </Suspense>
+      </Canvas>
+      )
+    </div>
   );
 };
 
