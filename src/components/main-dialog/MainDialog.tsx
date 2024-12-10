94c06b650513da7ee462f44997bc32a2a2b21699
Cristian David <cristian.pacheco@correounivalle.edu.co>
2024-12-10 12:56:47 -0500

    Fix the postal mitmatch names and routes

--- src/components/main-dialog/MainDialog.tsx
+++ src/components/main-dialog/MainDialog.tsx
@@ -1,32 +1,40 @@
 import { Html } from "@react-three/drei";
 import { Vector3 } from "three";
 
-interface Props {
-  title: string | null;
-  say: string;
-  position: number[];
-  onClickHandler: () => void;
+interface Style {
+	[props: string]: string | number;
 }
 
-const MainDialog = ({ say, position, onClickHandler, title }: Props) => (
-  <Html
-    // Adjust position as needed
-    position={new Vector3(position[0], position[1], position[2])}
-    wrapperClass="html-dialog" // Optional: Add a class for styling
-    center // Center the HTML content
-  >
-    <div className="absolute w-80 b-0 r-0 bg-white/[0.25] rounded-md p-4">
-      <p className="text-7xl ms-0 text-[#000000] font-bold">{title}</p>
-      {/* You can add a title if needed */}
-      <p className="text-white text-base">{say}</p>
-      <button
-        className="bg-red-500 text-white rounded-md p-2 px-4"
-        onClick={(e) => onClickHandler()}
-      >
-        Explorar
-      </button>
-    </div>
-  </Html>
+interface Props {
+	title: string | null;
+	say: string;
+	position: number[];
+	onClickHandler: () => void;
+	style?: Style;
+}
+
+const MainDialog = ({ say, position, onClickHandler, title, style }: Props) => (
+	<Html
+		// Adjust position as needed
+		position={new Vector3(position[0], position[1], position[2])}
+		wrapperClass="html-dialog" // Optional: Add a class for styling
+		center // Center the HTML content
+	>
+		<div
+			className="absolute w-80 b-0 r-0 bg-white/[0.25] rounded-md p-4"
+			style={style}
+		>
+			<p className="text-5xl ms-0 text-[#000000] font-bold">{title}</p>
+			{/* You can add a title if needed */}
+			<p className="text-white text-base">{say}</p>
+			<button
+				className="bg-red-500 text-white rounded-md p-2 px-4"
+				onClick={(e) => onClickHandler()}
+			>
+				Explorar
+			</button>
+		</div>
+	</Html>
 );
 
 export default MainDialog;
--- src/pages/Home/EnvironmentalCare.jsx
+++ src/pages/Home/EnvironmentalCare.jsx
@@ -2,15 +2,15 @@ import React from "react";
 import "./EnvironmentalCare.css"; // Asegúrate de que la ruta sea correcta
 
 const CuidadoMedioAmbiente = () => (
-  <div className="cuidado-medio-container w-96">
-    <h2>Importancia de Cuidar el Medio Ambiente</h2>
-    <p className="">
-      Cuidar nuestro medio ambiente es crucial para garantizar la salud y
-      bienestar de las futuras generaciones. Pequeñas acciones, como reducir el
-      uso de plásticos, reciclar y conservar agua, pueden tener un gran impacto.
-      ¡Cada uno de nosotros puede hacer la diferencia!
-    </p>
-  </div>
+	<div className="cuidado-medio-container w-80">
+		<h2>Importancia de Cuidar el Medio Ambiente</h2>
+		<p className="">
+			Cuidar nuestro medio ambiente es crucial para garantizar la salud y
+			bienestar de las futuras generaciones. Pequeñas acciones, como reducir el
+			uso de plásticos, reciclar y conservar agua, pueden tener un gran impacto.
+			¡Cada uno de nosotros puede hacer la diferencia!
+		</p>
+	</div>
 );
 
 export default CuidadoMedioAmbiente;
